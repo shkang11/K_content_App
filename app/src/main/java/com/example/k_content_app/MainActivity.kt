637f84be330709e0ca0f657ecd6b47a731035120
swchois <tmddnjs3283@gmail.com>
2024-04-25 16:24:31 +0900

    4/25 학습후 커밋

--- app/src/main/java/com/example/k_content_app/MainActivity.kt
+++ app/src/main/java/com/example/k_content_app/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.k_content_app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val img1 = findViewById<ImageView>(R.id.img1)
+        img1.setOnClickListener {
+            Toast.makeText(this,"사진 클릭",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ImgActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
