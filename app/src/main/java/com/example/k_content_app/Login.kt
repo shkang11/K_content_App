170489f77257e8e452028345d8efcf42f647ad63
swchois <tmddnjs3283@gmail.com>
2024-04-25 17:48:35 +0900

    4/25 로그인화면 제작중

--- app/src/main/java/com/example/k_content_app/Login.kt
+++ app/src/main/java/com/example/k_content_app/Login.kt
@@ -1,34 +1,20 @@
 package com.example.k_content_app
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val img1 = findViewById<ImageView>(R.id.img1)
-        img1.setOnClickListener {
-            Toast.makeText(this,"사진 클릭",Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, ImgActivity::class.java)
-            startActivity(intent)
-        }
-        val img2 = findViewById<ImageView>(R.id.img2)
-        img2.setOnClickListener {
-            val intent = Intent(this,Login::class.java)
-            startActivity(intent)
         }
     }
 }
