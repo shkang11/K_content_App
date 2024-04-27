3937aac883673637e6edeaba6de38b7d9b272022
swchois <tmddnjs3283@gmail.com>
2024-04-27 11:46:45 +0900

    4/27 회원 가입 폼 만들기, 회원 가입 선택 XML 생성

--- app/src/main/java/com/example/k_content_app/GeneralEnroll.kt
+++ app/src/main/java/com/example/k_content_app/GeneralEnroll.kt
@@ -1,29 +1,20 @@
 package com.example.k_content_app
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class GeneralEnroll : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_general_enroll)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val enrollBtn = findViewById<Button>(R.id.enrollBtn)
-        enrollBtn?.setOnClickListener {
-            val intent = Intent(this,SelectEnroll::class.java)
-            startActivity(intent)
         }
     }
 }
