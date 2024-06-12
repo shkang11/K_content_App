e7abd7691dce4cc700b995f3c681f87020e95552
GyoungTae <83820089+KimGyoungTae@users.noreply.github.com>
2024-06-12 16:31:14 +0900

    - 2024.06.12
    UploadChooser에 대한 인터페이스를 만든 후 관련 이벤트 메소드들을
    다른 클래스에게 접근할 수 있도록.. 작업 진행.
    
    이미지 검색 서칭 이후 작업은 다른 클래스에서 처리하기 때문에
    인터페이스 활용하는 이 작업이 필요함.

--- app/src/main/java/com/example/k_content_app/SearchingFragment.kt
+++ app/src/main/java/com/example/k_content_app/SearchingFragment.kt
@@ -23,6 +23,7 @@ class SearchingFragment : Fragment(), ImageModel.ImageSearchCallback {
 
     private var resView: TextView? = null
     private var imageView: ImageView? = null
+    private var uploadChooser: UploadChooser? = null
 
     private val imagePickerLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == AppCompatActivity.RESULT_OK) {
@@ -32,6 +33,7 @@ class SearchingFragment : Fragment(), ImageModel.ImageSearchCallback {
                     val bitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver, it)
                     imageView?.setImageBitmap(bitmap)
                     imageModel.modelActivity(bitmap)
+                    uploadChooser?.dismiss()
                     Log.d("ImageSearch", "Image loaded and processed successfully")
                 } catch (e: Exception) {
                     Log.e("ImageSearch", "Error loading image", e)
@@ -51,9 +53,32 @@ class SearchingFragment : Fragment(), ImageModel.ImageSearchCallback {
         resView = view.findViewById(R.id.searchcontent)
         imageView = view.findViewById(R.id.imageSearchView)
 
+
         // ImageModel 초기화
         imageModel = ImageModel(requireContext())
         imageModel.callback = this  // Set the callback
+
+        // 이미지 검색 버튼
+        val imageSearchButton = view.findViewById<Button>(R.id.imageSearchBtn)
+        imageSearchButton.setOnClickListener {
+            // 로그 추가
+            Log.d("ImageSearch", "Click ImageSearch Button")
+           // UploadChooser().show(parentFragmentManager, "UploadChooser")
+           uploadChooser =  UploadChooser().apply {
+                addInterface(object :UploadChooser.UploadChooserInterface{
+                    override fun cameraOnClick() {
+                        Log.d("upload","cameraOnClick")
+                    }
+
+                    override fun galleryOnClick() {
+                        Log.d("upload","galleryOnclick")
+                        callImageSearch()
+                    }
+                    })
+                }
+                uploadChooser!!.show(parentFragmentManager, "UploadChooser")
+            }
+
 
         // UserInfoFragment로 이동하는 버튼
         view.findViewById<Button>(R.id.btn2).setOnClickListener {
@@ -71,32 +96,26 @@ class SearchingFragment : Fragment(), ImageModel.ImageSearchCallback {
             startActivity(intent)
         }
 
-        // 이미지 검색 버튼
-        val imageSearchButton = view.findViewById<Button>(R.id.imageSearchBtn)
-        imageSearchButton.setOnClickListener {
-            // 로그 추가
-            Log.d("ImageSearch", "Click ImageSearch Button")
-            callImageSearch(this)
-        }
-
         return view
     }
 
     override fun onImageSearchResult(result: String) {
         Log.d("ImageSearch", "onImageSearchResult called with result: $result")
         resView?.text = result
+
+        // 이미지 선택 후 검색 결과를 확인 하지 않고, 바로 다음 Step으로 넘기고 싶을 때
 //        val searchText = result
 //        val intent = Intent(activity, SearchActivity::class.java)
 //        intent.putExtra("searchText", searchText)
 //        startActivity(intent)
     }
 
-    fun callImageSearch(fragment: Fragment) {
+    fun callImageSearch() {
         Log.d("ImageSearch", "callImageSearch called")
-        imageSelect(fragment)
+        imageSelect()
     }
 
-    fun imageSelect(fragment: Fragment) {
+    fun imageSelect() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "image/*"
         Log.d("ImageSearch", "Launching image selector")
